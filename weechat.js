9d90ec1cb022ab39a0f4a30295691038f7ef5151
eirikb <eirikb@eirikb.no>
2012-01-24 23:14:03 +0100

    Pointers come in groups...

--- weechat.js
+++ weechat.js
@@ -3,8 +3,13 @@ var net = require('net');
 var client, partial, total = 0,
 data;
 
+exports.connect = function(host, port, password) {};
+
+exports.write = function(msg) {
+	client.write(msg + '\n');
+};
+
 client = net.connect(8000, function() {
-	console.log('client connected');
 	client.write('init password=test,compression=off\n');
 	//client.write('info version\n');
 	//client.write('input irc.freenode.#testfrest hello guys!\n');
@@ -13,7 +18,8 @@ client = net.connect(8000, function() {
 	//client.write('hdata buffer:gui_buffers(*)/lines/first_line(*)/data\n');
 	//client.write('input core.weechat /help filter\n');
 	//client.write('hdata buffer *\n');
-	client.write('hdata buffer:gui_buffers(*) number,name\n');
+	//client.write('hdata buffer:gui_buffers(*) number,name\n');
+	client.write('nicklist\n');
 });
 
 client.on('data', function(part) {
@@ -36,7 +42,7 @@ client.on('data', function(part) {
 		total = 0;
 		partial = '';
 		var ret = parse();
-        console.log('RET', ret);
+		console.log('RET', ret);
 	}
 });
 
@@ -47,11 +53,21 @@ client.on('end', function() {
 var hack = {
 	hda: getHdata,
 	str: getString,
-	int: getInt
+	int: getInt,
+	inf: getInf,
+	ptr: getPointer,
+	tim: getPointer,
+	chr: getChar
 };
 
 function parse() {
-	var type = data.slice(0, 3);
+	var type;
+
+	if (data.length < 3) {
+		return null;
+	}
+
+	type = data.slice(0, 3);
 	data = data.slice(3);
 
 	if (!hack[type]) {
@@ -68,46 +84,69 @@ function getCompression() {
 }
 
 function getInt() {
-	var l = ((data[0] & 0xff) << 24) | ((data[1] & 0xff) << 16) | ((data[2] & 0xff) << 8) | (data[3] & 0xff);
+	var i = ((data[0] & 0xff) << 24) | ((data[1] & 0xff) << 16) | ((data[2] & 0xff) << 8) | (data[3] & 0xff);
 	data = data.slice(4);
-	return l > 0 ? l: null;
+	return i > 0 ? i: null;
 }
 
 function getString() {
-	var l = getInt();
+	var l = getInt(),
 	s = data.slice(0, l);
 	data = data.slice(l);
 	return s.toString();
 }
 
 function getPointer() {
-    var l = data[0],
-    pointer = data.slice(1, l + 1);
-    data = data.slice(l + 1);
-    return pointer;
+	var l = data[0],
+	pointer = data.slice(1, l + 1);
+	data = data.slice(l + 1);
+	return pointer;
 }
 
 function getHdata() {
-	var i, p, tmp, obj = {},
-	hpath = getString(),
-	keys = getString().split(','),
-	count = getInt(),
-    objs = [];
+	var i, p, tmp, keys, obj = {},
+	objs = [];
 
-    keys = keys.map(function(key) {
-        return key.split(':');
-    });
+	obj.hpath = getString();
+	keys = getString().split(',');
+	obj.count = getInt();
 
-    for (i = 0; i < count; i++ ) {
-        p = getPointer();
-        tmp = {};
-        keys.forEach(function(key) {
-            tmp[key[0]] = hack[key[1]]();
-        });
-        objs.push(tmp);
-    }
-    obj.objects = objs;
+	keys = keys.map(function(key) {
+		return key.split(':');
+	});
+
+	for (i = 0; i < obj.count; i++) {
+		tmp = {
+			pointers: []
+		};
+		for (p = 0; p < obj.hpath.split('/').length; p++) {
+			tmp.pointers.push(getPointer());
+		}
+		keys.forEach(function(key) {
+			var type = hack[key[1]];
+			if (type) {
+				tmp[key[0]] = hack[key[1]]();
+			} else {
+				console.log('OOOO MGMGMG!', key[1]);
+			}
+		});
+		objs.push(tmp);
+	}
+	obj.objects = objs;
 
 	return obj;
+}
+
+function getInf() {
+	return {
+		name: getString(),
+		content: getString()
+	};
+}
+
+function getChar() {
+	var c = data[0];
+	data = data.slice(1);
+	return c;
 }
 
