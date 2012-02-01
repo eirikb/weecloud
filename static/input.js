aba83b783658e283b93bdaf716ad10c57b0eee62
eirikb <eirikb@eirikb.no>
2012-02-01 21:38:39 +0100

    buffer -> history to prevent confusion

--- static/input.js
+++ static/input.js
@@ -3,36 +3,36 @@ weecloud.input = (function() {
 
     $(function() {
         $input = $('input'),
-        buffer = [],
-        bufferPos = 0;
+        history = [],
+        historyPos = 0;
 
         $input.keydown(function(e) {
-            var line, currentPos = bufferPos;
+            var line, currentPos = historyPos;
             switch (e.keyCode) {
             case 13:
                 line = $input.val();
                 if (line.length > 0) {
                     $input.val('');
-                    buffer.push(line);
-                    bufferPos = buffer.length;
+                    history.push(line);
+                    historyPos = history.length;
                     weecloud.main.msg(line);
                 }
                 return false;
             case 38:
-                bufferPos--;
+                historyPos--;
                 break;
             case 40:
-                bufferPos++;
+                historyPos++;
                 break;
             }
-            if (bufferPos < 0) {
-                bufferPos = 0;
-            } else if (bufferPos > buffer.length) {
-                bufferPos = buffer.length;
+            if (historyPos < 0) {
+                historyPos = 0;
+            } else if (historyPos > history.length) {
+                historyPos = history.length;
             }
 
-            if (currentPos !== bufferPos) {
-                $input.val(buffer[bufferPos]);
+            if (currentPos !== historyPos) {
+                $input.val(history[historyPos]);
                 return false;
             }
         });
