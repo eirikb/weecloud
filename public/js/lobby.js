e74d3060eecc2ffa8da68f021d8b83b54534df64
eirikb <eirikb@eirikb.no>
2012-02-04 16:02:00 +0100

    Remembering connections

--- public/js/lobby.js
+++ public/js/lobby.js
@@ -1,4 +1,7 @@
 $(function() {
+    var connections = getConnections(),
+    $remembered = $('tbody');
+
     $('[rel=popover]').popover();
 
     $('#insert-ip').click(function() {
@@ -6,5 +9,44 @@ $(function() {
 
         $host.val($host.data('host'));
     });
+
+    $('button:submit').click(function() {
+        var c, names = ['host', 'port', 'password'];
+
+        if ($(':checkbox').is(':checked')) {
+            connection = {};
+            $.each(names, function(i, name) {
+                connection[name] = $('[name=' + name + ']').val();
+            });
+            connections.push(connection);
+            localStorage.connections = JSON.stringify(connections);
+        }
+    });
+
+    $.each(connections, function(i, connection) {
+        var $tr = $('<tr>'),
+        $btn = $('<button>').addClass('btn');
+
+        $btn.text('Connect');
+        $btn.click(function() {
+            $.each(connection, function(name, val) {
+                $('[name=' + name + ']').val(val);
+            });
+            $('button:submit').click();
+        });
+
+        $tr.append('<td>' + connection.host);
+        $tr.append('<td>' + connection.port);
+        $tr.append($('<td>').append($btn));
+
+        $remembered.append($tr);
+    });
+
+    function getConnections() {
+        try {
+            return JSON.parse(localStorage.connections);
+        } catch(e) {}
+        return [];
+    }
 });
 
