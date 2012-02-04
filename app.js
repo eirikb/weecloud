cce28d4427d36fb0548f29d45764cc9aa7a997b2
eirikb <eirikb@eirikb.no>
2012-02-04 20:18:36 +0100

    Stuff I dont need

--- app.js
+++ app.js
@@ -15,16 +15,9 @@ app.configure(function() {
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
     app.use(express.bodyParser());
-    app.use(express.methodOverride());
+    //app.use(express.methodOverride());
     app.use(app.router);
     app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function() {
-    app.use(express.errorHandler({
-        dumpExceptions: true,
-        showStack: true
-    }));
 });
 
 app.configure('production', function() {
