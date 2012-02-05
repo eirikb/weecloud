0dafdc66b1f6409054fac01668b5551635f08404
eirikb <eirikb@eirikb.no>
2012-02-05 20:48:19 +0100

    Dev dump

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ app.configure(function() {
     app.use(express.static(__dirname + '/public'));
 });
 
+app.configure('development', function() {
+    app.use(express.errorHandler({
+        dumpExceptions: true,
+        showStack: true
+    }));
+});
+
 app.configure('production', function() {
     app.use(express.errorHandler());
 });
