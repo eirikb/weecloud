15b635d5ee39dab3a1e405b556e6a49dab4bd95f
Eirik Brandtzæg <eirikb@eirikb.no>
2012-03-21 09:47:00 +0100

    Set active

--- public/js/channels.js
+++ public/js/channels.js
@@ -1,12 +1,22 @@
 (function() {
+
+    function setActive($channel) {
+        $('#channels .active').removeClass('active');
+        $channel.addClass('active');
+    }
+
     socket.on('addBuffer', function(buffer) {
         var $channel = $('<a>').text(buffer.name);
 
         $channel.click(function() {
+            setActive($channel);
             buffers.show(buffer.id);
         });
 
+        setActive($channel);
+
         $('#channels').append($('<li>').append($channel));
     });
+
 })();
 
