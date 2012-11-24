85fbf3c1c170b8cca9c2986565be03fbe47515cf
Eirik Brandtzæg <eirikb@eirikb.no>
2012-11-24 16:29:36 +0000

    more stuff in test, no asserts

--- test/relay.js
+++ test/relay.js
@@ -1,16 +1,48 @@
 var io = require('socket.io-client');
+var requester = require('requester');
+var Requester = require('requester');
 
-var socketURL = 'http://0.0.0.0:3000';
+var requester = new Requester();
+var url = 'http://localhost:3000';
 // This is for connecting to a real WeeChat! (Integration test)
 var auth = require('./auth.json');
 
 var options = {
-    transports: ['websocket'],
-    'force new connection': true
+    transports: ['websocket']
 };
+
+Object.keys(io.sockets).forEach(function(id) {
+    console.log('disconnecting:', id);
+    io.sockets[id].disconnect();
+});
+io.sockets = {};
 
 describe("relay", function() {
     it('should connect to a WeeChat from auth', function() {
-        var client = io.connect(socketURL, options);
+        requester.post(url + '/relay', {
+            data: auth
+        }, function(data) {
+            var guid = data.match(/data-guid=\"(\S+)\"/)[1];
+
+            var client = io.connect(url, options);
+
+            client.on('error', function(err) {
+                console.log('Error: ', err);
+            });
+
+            client.on('connect', function() {
+                console.log('connected');
+                client.emit('sync', guid);
+            });
+
+            client.on('auth', function(res) {
+                console.log('auth', res);
+                client.emit('read:buffers');
+            });
+
+            client.on('buffers', function(buffers) {
+                console.log('buffers', buffers);
+            });
+        });
     });
 });
