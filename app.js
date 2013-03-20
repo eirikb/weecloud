ac9fd63a6cbda58c345efe8bba6838fc6c357d23
eirikb <eirikb@eirikb.no>
2013-03-20 23:54:56 +0100

    bumped package versions

--- app.js
+++ app.js
@@ -13,76 +13,76 @@ var pkg = require('./package.json');
 io.set('log level', 1);
 
 app.configure(function() {
-    app.set('view options', {
-        layout: false
-    });
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'jade');
-    app.use(express.bodyParser());
-    app.use(app.router);
-    app.use(express.static(__dirname + '/public'));
+  app.set('view options', {
+    layout: false
+  });
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function() {
-    app.use(express.errorHandler({
-        dumpExceptions: true,
-        showStack: true
-    }));
+  app.use(express.errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
 });
 
 app.configure('production', function() {
-    app.use(express.errorHandler());
+  app.use(express.errorHandler());
 });
 
 app.get('/', function(req, res) {
-    var host = req.socket.remoteAddress;
-    console.log(new Date(), host);
-    if (req.headers['x-forwarded-for']) {
-        host = req.headers['x-forwarded-for'];
-    }
-    res.render('lobby', {
-        version: pkg.version,
-        host: host
-    });
+  var host = req.socket.remoteAddress;
+  console.log(new Date(), host);
+  if (req.headers['x-forwarded-for']) {
+    host = req.headers['x-forwarded-for'];
+  }
+  res.render('lobby', {
+    version: pkg.version,
+    host: host
+  });
 });
 
 app.get('/relay', function(req, res) {
-    res.render('relay', {
-        guid: ''
-    });
+  res.render('relay', {
+    guid: ''
+  });
 });
 
 app.post('/relay', function(req, res) {
-    var g, b = req.body;
+  var g, b = req.body;
 
-    if (b.host && b.port && b.password) {
-        g = guid();
-        refs[g] = b;
-        res.render('relay', {
-            guid: g
-        });
-    } else {
-        res.redirect('/');
-    }
+  if (b.host && b.port && b.password) {
+    g = guid();
+    refs[g] = b;
+    res.render('relay', {
+      guid: g
+    });
+  } else {
+    res.redirect('/');
+  }
 });
 
 io.sockets.on('connection', function(socket) {
-    socket.on('sync', function(g) {
-        var data = refs[g];
-        if (data) {
-            weecloud.init(socket, data);
-        } else {
-            socket.emit('error', 'Unkown guid: ' + g);
-        }
-    });
+  socket.on('sync', function(g) {
+    var data = refs[g];
+    if (data) {
+      weecloud.init(socket, data);
+    } else {
+      socket.emit('error', 'Unkown guid: ' + g);
+    }
+  });
 });
 
 function S4() {
-    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+  return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
 }
 
 function guid() {
-    return S4() + S4() + S4();
+  return S4() + S4() + S4();
 }
 
 server.listen(port);
--- weecloud.js
+++ weecloud.js
@@ -1,117 +1,84 @@
-var WeeChat = require('weechat');
-var sanitize = require('validator').sanitize;
+var weechat = require('weechat');
+var util = require('util');
 
-var handlers = {};
-
-function getHandler(data) {
-    var h = handlers;
-
-    ['host', 'port', 'password'].forEach(function(n) {
-        if (h) h = h[data[n]];
-    });
-    return h;
-}
-
-function putHandler(data, handler) {
-    var h = handlers[data.host] = handlers[data.host] || {};
-    h = h[data.port] = {};
-    h[data.password] = handler;
-}
+var buffers = 'hdata buffer:gui_buffers(*) number,short_name,title,local_variables';
+var lines = 'hdata buffer:%s/own_lines/last_line(-%d)/data';
 
 exports.init = function(socket, data) {
-    var handler = getHandler(data);
-    var weeChat = new WeeChat();
 
+  var client = weechat.connect(data.host, data.port, data.password, function(err) {
 
-    if (!handler) {
-        weeChat.connect(data.host, data.port, data.password, function(err) {
-            if (!err) {
-                handler = new Handler(weeChat);
-                putHandler(data, handler);
-                handler.addSocket(socket);
-                socket.emit('auth');
-            } else {
-                socket.emit('error', 'Oh noes, errors! :(   -   ' + err);
-            }
-        });
-        weeChat.on('error', function(err) {
-            socket.emit('error', 'Something borked  -  ' + err);
-            console.error(err);
-        });
-    } else {
-        handler.addSocket(socket);
+    if (err) {
+      socket.emit('error', err);
+      return;
     }
-};
 
-function getMessage(w, message) {
-    message = w.style(message).map(function(part) {
-        part.text = sanitize(part.text).entityEncode();
+    function getBuffer(buffer) {
+      if (buffer && buffer.pointers) {
+        buffer.id = buffer.pointers[0];
+        return buffer;
+        emit('buffer', buffer);
+      } else {
+        console.error('Buffer has no pointers: ', buffer);
+        return null;
+      }
+    }
+
+    function getLine(line) {
+      return {
+        bufferid: line.buffer,
+        from: weechat.style(line.prefix),
+        date: line.date,
+        message: getMessage(line.message)
+      };
+    }
+
+    function getMessage(message) {
+      message = weechat.style(message).map(function(part) {
         return part;
-    });
-    return message;
-}
-
-function Handler(weeChat) {
-    if (! (this instanceof Handler)) return new Handler(weeChat);
-    var sockets = [];
-
-    function emit(a, b) {
-        sockets.forEach(function(socket) {
-            socket.emit(a, b);
-        });
+      });
+      return message;
     }
 
-    weeChat.on('open', function(buffers) {
-        buffers.forEach(function(buffer) {
-            if (buffer && buffer.pointers) {
-                buffer.id = buffer.pointers[0];
-                emit('buffer', buffer);
-            } else {
-                console.error('Buffer has no pointers: ', buffer);
-            }
-        });
+    client.on('open', function(buffers) {
+      buffers.forEach(function(buffer) {
+        buffer = getBuffer(buffer);
+        if (buffer) socket.emit('buffer', buffer);
+      });
     });
 
-    weeChat.on('close', function(buffers) {
-        buffers.forEach(function(buffer) {
-            emit('closeBuffer', buffer.buffer);
-        });
+    client.on('close', function(buffers) {
+      buffers.forEach(function(buffer) {
+        emit('closeBuffer', buffer.buffer);
+      });
     });
 
-    weeChat.on('line', function(lines) {
-        lines.forEach(function(line) {
-            emit('msg', {
-                bufferid: line.buffer,
-                from: weeChat.style(line.prefix),
-                date: line.date,
-                message: getMessage(weeChat, line.message)
-            });
-        });
+    client.on('line', function(lines) {
+      if (!Array.isArray(lines)) lines = [lines];
+      lines.forEach(function(line) {
+        emit('line', getLine(line));
+      });
     });
 
-    this.addSocket = function(socket) {
-        sockets.push(socket);
+    socket.on('msg', function(msg) {
+      client.write('input ' + msg.id + ' ' + msg.line);
+    });
 
-        socket.on('msg', function(msg) {
-            weeChat.write('input ' + msg.id + ' ' + msg.line);
+    socket.emit('auth', true);
+    socket.emit('synced', 'done');
+
+    client.send(buffers, function(buffers) {
+      buffers.forEach(function(buffer) {
+        buffer.id = buffer.pointers[0];
+        var query = util.format(lines, buffer.id, 20);
+
+        client.send(query, function(lines) {
+          buffer = getBuffer(buffer);
+          buffer.lines = lines.map(getLine);
+          socket.emit('buffer', buffer);
         });
-
-        socket.emit('auth', true);
-
-        // Only 30 last lines
-        weeChat.bufferlines(30, function(buffers) {
-            buffers.forEach(function(buffer) {
-                buffer.lines = buffer.lines.map(function(line) {
-                    return {
-                        prefix: weeChat.style(line.prefix),
-                        date: line.date,
-                        message: getMessage(weeChat, line.message)
-                    };
-                });
-                socket.emit('buffer', buffer);
-            });
-            socket.emit('synced', 'done');
-        });
-    };
-}
-
+      });
+    });
+    return;
+  });
+};
