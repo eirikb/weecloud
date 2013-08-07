ad8bffe4f1d934aeb6652160d84ae364edd98868
eirikb <eirikb@eirikb.no>
2013-08-07 23:07:11 +0200

    Login window

--- js/connect.js
+++ js/connect.js
@@ -39,8 +39,10 @@ $(function() {
         ssl: ssl,
         password: password
       }, function(version) {
-        _gaq.push(['_setCustomVar', 1, 'WeeChat version', version]);
-        _gaq.push(['_trackEvent', 'WeeChat', 'WeeChat version']);
+        if (window._gaq) {
+          _gaq.push(['_setCustomVar', 1, 'WeeChat version', version]);
+          _gaq.push(['_trackEvent', 'WeeChat', 'WeeChat version']);
+        }
 
         $('#connect').hide();
         $('#center, #input, #top').removeClass('hide');
