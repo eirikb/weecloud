5ac9393789f4aa906b5dc31c83eb37506c4b355c
eirikb <eirikb@eirikb.no>
2013-08-28 22:39:22 +0200

    Connect with Syphon

--- js/connect.js
+++ js/connect.js
@@ -19,13 +19,23 @@ App.module('Connect', function(Connect, App, Backbone) {
     },
 
     submit: function() {
+      var data = Backbone.Syphon.serialize(this);
+      this.model.set(data);
+
+      if (data.remember) localStorage.connect = JSON.stringify(model.toJSON());
+      else localStorage.connect = '';
+
       this.model.save();
       return false;
     }
   });
 
-
   var model = new Connect.Model({});
+  try {
+    var data = JSON.parse(localStorage.connect);
+    model.set(data);
+  } catch (e) {}
+
   var view = new Connect.View({
     model: model
   });
